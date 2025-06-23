1f42a4275f2c2658f37b30c22ab52984157409d5
data-mad123 <farzana.khanom@natwest.com>
2025-06-23 09:43:12 +0100

    Update date.sql

--- date.sql
+++ date.sql
@@ -1,6 +1,6 @@
- CREATE TABLE IF NOT EXISTS date (
+ CREATE TABLE IF NOT EXISTS date_code (
          sas_date INT,
          converted_date DATE
      )
      ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
-     LOCATION 's3://myawsbucketfk11/date/';
+     LOCATION 's3://myawsbucketfk11/date_code/';
